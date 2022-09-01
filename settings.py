ac9f108968bfb5137c6a7d6772a96853f84f72c4
Michael Morar <mmorar@bink.com>
2022-09-01 14:47:45 +0100

    fixed amex URL

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ class Settings(BaseSettings):
     keyvault_url: str
     auth_token: str = "25556b98-9488-4c53-b0c6-682ae562321e"
     amex_auth_token: str = "5ca1870e-69a9-41b8-bbee-d7ee7d14f502"
-    amex_hostname: str = "https://api.americanexpress.com"
+    amex_hostname: str = "https://apigateway2s.americanexpress.com"
 
 settings = Settings()
