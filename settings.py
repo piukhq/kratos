eb58802a0cbc742ea9ebe183c6291e24fbe62263
Michael Morar <mmorar@bink.com>
2022-12-01 13:10:25 +0000

    fixed community code

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings, Field
 class Settings(BaseSettings):
     keyvault_url: str
     visa_auth_token: str = "25556b98-9488-4c53-b0c6-682ae562321e"
-    visa_community_code: str = "BINKCTE01"
+    visa_community_code: str = "BINK"
     amex_auth_token: str = "5ca1870e-69a9-41b8-bbee-d7ee7d14f502"
     amex_hostname: str = "https://apigateway2s.americanexpress.com"
 
