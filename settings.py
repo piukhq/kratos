87772b7787acb6944ff117e624e5b642f867e136
Michael Morar <mmorar@bink.com>
2022-12-01 12:33:37 +0000

    removed env variable

--- settings.py
+++ settings.py
@@ -5,7 +5,6 @@ class Settings(BaseSettings):
     keyvault_url: str
     visa_auth_token: str = "25556b98-9488-4c53-b0c6-682ae562321e"
     visa_community_code: str = "BINKCTE01"
-    visa_hostname: str = Field(..., env="visa_hostname")
     amex_auth_token: str = "5ca1870e-69a9-41b8-bbee-d7ee7d14f502"
     amex_hostname: str = "https://apigateway2s.americanexpress.com"
 
